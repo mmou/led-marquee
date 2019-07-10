604dab70424d0614abce855dfcc0b4c3449cf027
Merry Mou <moumerry@gmail.com>
2019-07-09 20:27:38 -0700

    what was displayed at priceless

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ use embedded_graphics::prelude::*;
 use embedded_graphics::Drawing;
 use embedded_graphics::{egline, primitives::Line, style::Style};
 use embedded_graphics::{fonts::Font8x16, prelude::*, text_8x16};
+use rand::distributions::Uniform;
+use rand::prelude::*;
 use rosc::OscPacket;
 use rpi_led_matrix::{LedCanvas, LedColor, LedFont, LedMatrix, LedMatrixOptions};
 use std::env;
+use std::f64::consts::PI;
 use std::marker::PhantomData;
 use std::net::{SocketAddrV4, UdpSocket};
 use std::path::Path;
 use std::str::FromStr;
-use std::{thread, time};
-use rand::distributions::Uniform;
-use rand::prelude::*;
-use std::time::Duration;
 use std::sync::{Arc, Mutex};
-use std::f64::consts::PI;
+use std::time::Duration;
+use std::{thread, time};
 
 const SCROLL_WAIT_NS: u32 = 5000000; // 0.05 sec
 
@@ -75,7 +75,7 @@ where
             self.display.draw(image.clone());
             self.display.screen.flush();
             x = x + 1;
-            thread::sleep(time::Duration::new(0, SCROLL_WAIT_NS * 10));
+            //thread::sleep(time::Duration::new(0, SCROLL_WAIT_NS * 10));
         }
     }
 }
@@ -132,7 +132,7 @@ where
     }
 
     pub fn inc_x(&mut self, x: i32) {
-        self.offset_x = self.offset_x + x;
+        self.offset_x = (self.offset_x + x) % self.max_x as i32;
     }
 
     pub fn set_x(&mut self, x: i32) {
@@ -140,7 +140,10 @@ where
     }
 
     pub fn set_image_width(&mut self, x: u32) {
-        self.max_x = x;
+        self.max_x = 32 * 4;
+        if x > 32 * 4 {
+            self.max_x = x;
+        }
     }
 }
 
@@ -180,7 +183,7 @@ impl Hub75 {
         options.set_cols(32);
         options.set_multiplexing(3);
         options.set_row_address_type(2);
-        options.set_brightness(70);
+        options.set_brightness(40);
         //options.set_pwm_lsb_nanoseconds(130);
         //options.set_inverse_colors(true);
         //options.set_refresh_rate(true);
@@ -239,33 +242,77 @@ impl Hub75 {
             self.draw(&temp_image_left);
             self.flush();
             x = x + 1;
-            thread::sleep(time::Duration::new(0, SCROLL_WAIT_NS * 10));
+            thread::sleep(time::Duration::new(0, SCROLL_WAIT_NS));
         }
     }
 }
 
 fn main() {
+    loop {
+        megaglitch(time::Duration::new(30, 0));
+
+        text(" A MESSAGE FROM  ".to_string(), time::Duration::new(2, 0));
+        display_market(time::Duration::new(9, 0));
+
+        for i in 1..4 {
+            text("      BUY NOW  ".to_string(), time::Duration::new(1, 0));
+            text("".to_string(), time::Duration::new(0, 500000000));
+        }
+
+        display_clue(time::Duration::new(24, 0));
+    }
+}
+
+fn text(text: String, duration: time::Duration) {
     let mut screen = Hub75::new();
 
-    let color = Rgb888::new(100, 0, 0);
+    let color = Rgb888::new(0, 100, 0);
     let msg = DisplayMessage {
-        text: "IT'S WORKING".to_string(),
-        color: color,
-        duration: time::Duration::new(5, 0),
+        text,
+        color,
+        duration,
     };
-    //screen.scroll_text_8x16(msg);
+    screen.draw_text_8x16(msg);
+}
 
-    /*
-    let mut image1: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp.bmp")).unwrap();
-    let mut image2: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../priceless2.bmp")).unwrap();
+fn display_clue(duration: time::Duration) {
+    let mut screen = Hub75::new();
+    let mut s = Scrollable::<Hub75, Rgb888>::new(screen);
+    let mut marquee = Marquee::<Hub75, Rgb888>::new(s);
 
+    let mut clue: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../clue.bmp")).unwrap();
+    marquee.scroll(&clue, clue.width() + 6, duration);
+}
+
+fn display_market(duration: time::Duration) {
+    let mut screen = Hub75::new();
+    let mut s = Scrollable::<Hub75, Rgb888>::new(screen);
+    let mut marquee = Marquee::<Hub75, Rgb888>::new(s);
+
+    let mut market: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../market.bmp")).unwrap();
+    marquee.scroll(&market, market.width() + 12, duration);
+}
+
+fn clue_old(duration: time::Duration) {
+    let mut screen = Hub75::new();
+    let mut clue1: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../clue1.bmp")).unwrap();
+    let mut clue2: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../clue2.bmp")).unwrap();
+    let mut clue3: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../clue3.bmp")).unwrap();
+    let mut clue4: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../clue4.bmp")).unwrap();
     let mut s = Scrollable::<Hub75, Rgb888>::new(screen);
 
     let mut marquee = Marquee::<Hub75, Rgb888>::new(s);
 
-    marquee.scroll(&image1, image1.width(), time::Duration::new(5, 0));
-    marquee.scroll(&image2, image2.width(), time::Duration::new(5, 0));
-    */
+    for i in 1..3 {
+        marquee.scroll(&clue1, clue1.width(), duration);
+        marquee.scroll(&clue2, clue2.width(), duration);
+        marquee.scroll(&clue3, clue3.width(), duration);
+        marquee.scroll(&clue4, clue4.width(), duration);
+    }
+}
+
+fn megaglitch(duration: time::Duration) {
+    let mut screen = Hub75::new();
 
     let mut image: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp.bmp")).unwrap();
     let mut image1: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp-1.bmp")).unwrap();
@@ -274,48 +321,129 @@ fn main() {
     let mut image4: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp-4.bmp")).unwrap();
     let mut image5: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp-5.bmp")).unwrap();
 
-    let mut images = vec![image1, image2, image3, image4, image5];
-
+    let mut images = Arc::new(vec![image, image1, image2, image3, image4, image5]);
+    let mut indices: Vec<usize> = vec![1, 2, 3, 4, 5];
 
     let mut s = Scrollable::<Hub75, Rgb888>::new(screen);
-    s.set_image_width(image.width());
+    s.set_image_width(images[0].width());
+
+    let mut rng = rand::thread_rng();
+
+    let now = time::Instant::now();
+
+    let clean_range = Uniform::new(100, 2000);
+    let glitch_range = Uniform::new(10, 150);
+
+    let s = Arc::new(Mutex::new(s));
+    let i = Arc::new(Mutex::new(0 as usize));
+
+    let cloned_s = s.clone();
+    let cloned_images = images.clone();
+    let cloned_i = i.clone();
+    thread::spawn(move || {
+        let s = cloned_s;
+        let i = cloned_i;
+        let images = cloned_images;
+        while now.elapsed() < duration {
+            {
+                let mut s = s.lock().unwrap();
+                let mut j = i.lock().unwrap();;
+                s.inc_x(-1);
+                s.draw(&images[*j]);
+                s.screen.flush();
+            }
+            thread::sleep(Duration::from_millis(8));
+        }
+    });
+
+    while now.elapsed() < duration {
+        {
+            let mut s = s.lock().unwrap();
+            let mut j = i.lock().unwrap();
+            *j = 0;
+            s.draw(&images[0]);
+            s.screen.flush();
+        }
+        thread::sleep(Duration::from_millis(rng.sample(clean_range)));
+
+        indices.shuffle(&mut rng);
+
+        for k in indices.iter() {
+            {
+                let mut s = s.lock().unwrap();
+                let mut j = i.lock().unwrap();
+                *j = *k;
+
+                s.draw(&images[*k]);
+                s.screen.flush();
+            }
+            thread::sleep(Duration::from_millis(rng.sample(glitch_range)));
+        }
+    }
+}
+
+fn infinimegaglitch() {
+    let mut screen = Hub75::new();
+
+    let mut image: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp.bmp")).unwrap();
+    let mut image1: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp-1.bmp")).unwrap();
+    let mut image2: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp-2.bmp")).unwrap();
+    let mut image3: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp-3.bmp")).unwrap();
+    let mut image4: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp-4.bmp")).unwrap();
+    let mut image5: ImageBmp<Rgb888> = ImageBmp::new(include_bytes!("../megacorp-5.bmp")).unwrap();
+
+    let mut images = Arc::new(vec![image, image1, image2, image3, image4, image5]);
+    let mut indices: Vec<usize> = vec![1, 2, 3, 4, 5];
+
+    let mut s = Scrollable::<Hub75, Rgb888>::new(screen);
+    s.set_image_width(images[0].width());
 
     let mut rng = rand::thread_rng();
 
     let clean_range = Uniform::new(100, 2000);
-    let glitch_range = Uniform::new(10, 300);
+    let glitch_range = Uniform::new(10, 150);
 
-    let v = Arc::new(Mutex::new(s));
+    let s = Arc::new(Mutex::new(s));
+    let i = Arc::new(Mutex::new(0 as usize));
 
-    let cloned_v = v.clone();
-    let cloned_image = image.clone();
+    let cloned_s = s.clone();
+    let cloned_images = images.clone();
+    let cloned_i = i.clone();
     thread::spawn(move || {
-       let v = cloned_v;
+        let s = cloned_s;
+        let i = cloned_i;
+        let images = cloned_images;
         loop {
-           {
-               let mut s = v.lock().unwrap();
-               s.draw(&cloned_image);
-               s.screen.flush();
-           }
-            thread::sleep(Duration::from_millis(5));
+            {
+                let mut s = s.lock().unwrap();
+                let mut j = i.lock().unwrap();;
+                s.inc_x(-1);
+                s.draw(&images[*j]);
+                s.screen.flush();
+            }
+            thread::sleep(Duration::from_millis(8));
         }
     });
 
     loop {
         {
-            let mut s = v.lock().unwrap();
-            s.draw(&image);
+            let mut s = s.lock().unwrap();
+            let mut j = i.lock().unwrap();
+            *j = 0;
+            s.draw(&images[0]);
             s.screen.flush();
         }
         thread::sleep(Duration::from_millis(rng.sample(clean_range)));
 
+        indices.shuffle(&mut rng);
 
-        images.shuffle(&mut rng);
-
-        for img in images.iter() {
+        for k in indices.iter() {
             {
-                let mut s = v.lock().unwrap();
-                s.draw(img);
+                let mut s = s.lock().unwrap();
+                let mut j = i.lock().unwrap();
+                *j = *k;
+
+                s.draw(&images[*k]);
                 s.screen.flush();
             }
             thread::sleep(Duration::from_millis(rng.sample(glitch_range)));
